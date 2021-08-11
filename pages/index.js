ba7b8581db2ddbb4fa4c3c883cafb3a09a7fcc52
Rufus Pollock <rufus.pollock@datopian.com>
2021-08-11 10:45:45 +0200

    [lib,home,#3][l]: working connection to google sheets database and displaying summary for each org in list on front page.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
 import Head from 'next/head'
+import { getOrganizations } from "../lib/sheet";
 
-export default function Home() {
+
+export default function Home({ orgs }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen py-2">
       <Head>
         <title>State of Sensemaking 2021 - an Ecosystem Mapping - by Life Itself</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗺️</text></svg>" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
+      <main className="flex flex-col w-full flex-1 px-20">
+        <h1 className="text-6xl font-bold text-center my-8">
           State of Sensemaking
         </h1>
+        <section>
+          {orgs.map(({ title, description, url, imageurl }) => (
+            <article className="bg-white shadow overflow-hidden sm:rounded-lg mb-16 p-8">
+              <a
+                className="p-4"
+                key={title}
+                href={url}
+                target="_blank"
+              >
+                <h3 className="font-bold mb-2 text-4xl">{title}</h3>
+              </a>
+              <img src={imageurl} alt="" className="max-h-16" />
+              <div dangerouslySetInnerHTML={{ __html: description }} />
+            </article>
+          ))}
+        </section>
+
+        <div>{JSON.stringify(orgs[0],null,2)}</div>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
@@ -27,4 +47,13 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const orgs = await getOrganizations();
+  return {
+    props: {
+      orgs,
+    }
+  };
 }
