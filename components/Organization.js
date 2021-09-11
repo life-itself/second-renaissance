3c359bf9217d8cf152ef86d37c0c4e8793a36bb6
Rufus Pollock <rufus.pollock@datopian.com>
2021-09-12 00:06:30 +0200

    [profile][s]: make the page a little nicer.

--- components/Organization.js
+++ components/Organization.js
@@ -3,13 +3,33 @@ import Layout from '../components/Layout'
 export default function Organization({ org }) {
   return (
     <Layout title={org.title}>
-      <h1 className="text-6xl font-bold text-center my-8">
-        {org.title}
-      </h1>
-      <p><a href={org.url}>{org.url}</a></p>
-      <article>
-        {org.description}
-      </article>
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+        {/* Product details */}
+        <div className="lg:max-w-lg lg:self-end">
+          <div className="mt-4">
+            <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">{org.title}</h1>
+          </div>
+
+          <section aria-labelledby="information-heading" className="mt-4">
+            <h2 id="information-heading" className="sr-only">
+              Product information
+            </h2>
+
+            <div className="mt-4 space-y-6">
+              <p className="text-base text-gray-500">{org.description}</p>
+            </div>
+          </section>
+        </div>
+
+        {/* Product image */}
+        <div className="mt-10 lg:mt-0 lg:col-start-2 lg:row-span-2 lg:self-center">
+          <div className="aspect-w-1 aspect-h-1 rounded-lg overflow-hidden">
+            <img src={org.imageurl} alt={org.title} className="w-full h-full object-center object-cover" />
+          </div>
+        </div>
+      </div>
+    </div>
     </Layout>
   )
 }
