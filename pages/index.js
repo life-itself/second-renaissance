b563f629c59472650f1716a01928b4a6df3af80c
Rufus Pollock <rufus.pollock@datopian.com>
2021-09-11 23:40:17 +0200

    [home][m]: layout list of orgs in a nicer way using tailwindui product listing template.

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home({ orgs }) {
             <div className="text-center">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">State of Sensemaking</span>{' '}
-                <span className="block text-indigo-600 xl:inline">2021</span>
+                <span className="block text-yellow-300 xl:inline">2021</span>
               </h1>
               <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                 An ecosystem mapping by Life Itself
@@ -68,7 +68,7 @@ export default function Home({ orgs }) {
                 <div className="rounded-md shadow">
                   <a
                     href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md bg-yellow-300 hover:bg-yellow-400 md:py-4 md:text-lg md:px-10"
                   >
                     Download Report
                   </a>
@@ -88,20 +88,37 @@ export default function Home({ orgs }) {
       </div>
 
       <section className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        {orgs.map((org) => (
-          <article className="bg-white shadow overflow-hidden sm:rounded-lg mb-16 p-8">
-            <a
-              className="p-4"
-              key={org.title}
-              href={org.url}
-              target="_blank"
-            >
-              <h3 className="font-bold mb-2 text-4xl">{org.title}</h3>
-            </a>
-            <img src={org.imageurl} alt="" className="max-h-16" />
-            <div dangerouslySetInnerHTML={{ __html: org.description }} />
-          </article>
-        ))}
+        <div className="bg-white">
+          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Profiles</h2>
+
+            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+              {orgs.map((org) => (
+                <div key={org.slug} className="group relative">
+                  <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                    <img
+                      src={org.imageurl}
+                      alt={org.title}
+                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm font-medium text-gray-900">
+                        <a href={'@'+org.slug}>
+                          <span aria-hidden="true" className="absolute inset-0" />
+                          {org.title}
+                        </a>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">{org.url}</p>
+                      <p className="text-sm text-gray-700">{org.description.slice(0, 100) + '…'}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </section>
     </Layout>
   )
