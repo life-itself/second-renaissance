99e4aeb73537d471d18b5b89607dbc45c75627c9
Rufus Pollock <rufus.pollock@datopian.com>
2021-09-27 16:40:03 +0200

    [theme/fonts][m]: fixes and improvements to font setup - fonts now loading and displaying correctly.
    * Fonts weren't loading for me on remote website or locally because the fonts weren't actually being loaded / defined anywhere. Fixed this by reference at start of globals.css.
    * Use tailwind them config to set stuff default sans and serif and then apply them using reference to serif rather than explicit reference (cleaner)
    
    One issue is we are getting a flash of font change for Restora on headings. Could fix by preloading the font in the head tag. Something to think about.

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,9 @@ export default function Layout({ children, title='' }) {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗺️</text></svg>" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet" />
       </Head>
       <Nav /> 
       {children}
