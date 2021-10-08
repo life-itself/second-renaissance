535406cbe3fea929a9adffa35e39fc1b8920d6c3
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-08 08:40:27 +0200

    [#27,etl][m]: retrieve organization, activity and topic from spreadsheet and cache locally (and use for transform).
    * switch to using only json files (no need for csv)
    * bugfix: JSON.stringify strips keys with undefined value - we have that as some orgs don't have full info so fixed that to return ''

--- scripts/process.js
+++ scripts/process.js
@@ -1,17 +1,12 @@
 import {getMicroLinkInfo} from './sheet.js'
 import * as sheet from './sheet.js'
 
-// retrieve
-// data from google spreadsheet and cache in data/cache/orgs-spreadsheet.csv
-//
-// convert this to standard structure
-
-// extras
-// retrieve microlink
-// merge microlink
 
 (async() => {
-  // await getMicroLinkInfo()
+  // await sheet.retrieve()
   await sheet.transform()
+
+  // await getMicroLinkInfo()
+  // mergeMicroLink()
 })();
 
--- scripts/sheet.js
+++ scripts/sheet.js
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv/config'
 import { google } from 'googleapis'
 import {readFileSync} from 'fs'
 import {writeFileSync} from 'fs'
@@ -7,62 +8,39 @@ import  mql from '@microlink/mql'
 
 
 const DATA_DIR = './data'
+const DATA_PATHS = {
+  organization_cache: 'data/cache/organization.json',
+  activity_cache: 'data/cache/activity.json',
+  topic_cache: 'data/cache/topic.json',
+  organization: 'data/organization.json',
+  activity: 'data/activity.json',
+  topic: 'data/topic.json',
+}
 
 export async function transform() {
-  const file = readFileSync('data/cache/orgs-spreadsheet.csv', 'utf8');
-  const result = await parseCsvFile(file);
+  const result = JSON.parse(readFileSync(DATA_PATHS.organization_cache))
   function transformOrg(org) {
     return org
   }
-  const orgs = result.data.map(transformOrg)
-  writeFileSync(path.join(DATA_DIR, 'orgs.json'), JSON.stringify(orgs, null, 2))
-  // write out a sample of 20 of them
-  writeFileSync(path.join(DATA_DIR, 'orgs-sample.json'), JSON.stringify(orgs.slice(0,20), null, 2))
+  const orgs = result.map(transformOrg)
+  writeFileSync(DATA_PATHS.organization, JSON.stringify(orgs, null, 2))
 }
 
-const parseCsvFile = (rawFile) => new Promise((resolve, reject) => {
-  Papa.parse(rawFile, {
-    header: true,
-    complete: result => {
-      resolve(result);
-    },
-    error: function(error) {
-      reject(error);
-    }
-  });
-});
-
-async function getOrganizationsFromGoogleSheet() {
-  const scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-  const jwt = new google.auth.JWT(
-    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    null,
-    // we need to replace the escaped newline characters
-    // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
-    process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    scopes
-  );
-
-  const sheets = google.sheets({ version: "v4", auth: jwt });
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SPREADSHEET_ID,
-    range: "Organizations",
-  });
-
-  const rows = response.data.values;
-
-  if (rows.length) {
-    // first row is headers
-    const headers = rows.shift()
-
-    return rows.map((row) => {
-      return row.reduce(function(result, field, index) {
-        result[headers[index].toLowerCase()] = field;
-        return result;
-      }, {})
-    });
-  } else {
-    return [];
+/*
+ * Retrieve data from google sheets database to local cache.
+ *
+ * Only transformation is to lower case headers (in getGoogleSheet)
+ */
+export async function retrieve() {
+  for (const name of ['Organization', 'Activity', 'Topic']) {
+    const data = await getGoogleSheet(name)
+    const path = DATA_PATHS[name.toLowerCase() + '_cache']
+    // NB: JSON.stringify strips keys with undefined values so we have to convert them to ""
+    // https://stackoverflow.com/questions/26540706/preserving-undefined-that-json-stringify-otherwise-removes
+    writeFileSync(path, JSON.stringify(data,
+      (k, v) => v === undefined ? '' : v,
+      2)
+    )
   }
 }
 
@@ -87,3 +65,54 @@ export async function getMicroLinkInfo() {
     writeFileSync('../data/orgs-microlink.json', JSON.stringify(results, null, 2))
   }
 }
+
+// =================
+// Helpers
+
+async function getGoogleSheet(sheetName) {
+  const scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+  const jwt = new google.auth.JWT(
+    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    null,
+    // we need to replace the escaped newline characters
+    // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
+    process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    scopes
+  );
+
+  const sheets = google.sheets({ version: "v4", auth: jwt });
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: sheetName,
+  });
+
+  const rows = response.data.values;
+
+  if (rows.length) {
+    // first row is headers
+    const headers = rows.shift()
+
+    return rows.map((row) => {
+      var result = {}
+      for(const index in headers) {
+        result[headers[index].toLowerCase()] = row[index]
+      }
+      return result
+    });
+  } else {
+    return [];
+  }
+}
+
+const parseCsvFile = (rawFile) => new Promise((resolve, reject) => {
+  Papa.parse(rawFile, {
+    header: true,
+    complete: result => {
+      resolve(result);
+    },
+    error: function(error) {
+      reject(error);
+    }
+  });
+});
+
