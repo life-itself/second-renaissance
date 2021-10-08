0d50c33a2ba828604a00fd73545df42dfcc2699f
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-08 10:43:10 +0200

    [theme/nav][s]: add bunch of items to nav.

--- config/navLinks.js
+++ config/navLinks.js
@@ -1,5 +1,9 @@
 const navLinks = [
   { href: '/about', name: 'About' },
+  { href: '/contribute', name: 'Contribute' },
+  { href: '/glossary/', name: 'Glossary' },
+  { href: 'https://lifeitself.us/ecosystem/snapshot-2020/', name: '2020 Report' },
+  { href: 'https://lifeitself.us/contact', name: 'Contact' },
 ]
 
 export default navLinks
