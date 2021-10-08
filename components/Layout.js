a4f2b54edc746e4a592eaac2825b2d14903a64cb
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-08 12:50:07 +0200

    [theme/banner][m]: add banner with disclaimer about early stage nature of work.

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { XIcon } from '@heroicons/react/outline'
 
 import Nav from '../components/Nav'
 
@@ -15,6 +16,33 @@ export default function Layout({ children, title='' }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet" />
       </Head>
+      <div className="relative bg-yellow-300">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="pr-16 sm:text-center sm:px-16">
+            <p className="font-medium">
+              <span className="md:hidden">We announced a new product!</span>
+              <span className="hidden md:inline">
+                Note: this effort is still early stage and limited by our resources and knowledge.
+               </span>
+              <span className="block sm:ml-2 sm:inline-block">
+                <a href="about#disclaimer" className="font-bold underline">
+                  {' '}
+                  Learn more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
+            <button
+              type="button"
+              className="flex p-2 rounded-md hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-white"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
       <Nav /> 
       {children}
       <footer className="flex items-center justify-center w-full h-24 border-t">
