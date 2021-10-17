c6c57e24f2b5c9b071028b907ed9d2e5f85725d4
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-17 23:20:43 +0200

    [search,#15][s]: integrate search component into front page replacing simple org list.
    * Refactor pages/search.js into components/search.js and integrate into front page and back into pages/search.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { NextSeo } from 'next-seo'
 
 import { getOrganizations } from "../lib/db.js"
+import Search from "../components/search.js"
 import config from '../config/siteConfig'
 
 
@@ -112,34 +113,8 @@ export default function Home({ orgs }) {
       <section id="profiles" className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="bg-white">
           <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Profiles</h2>
-
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-              {orgs.map((org) => (
-                <div key={org.id} className="group relative">
-                  <div className="w-full min-h-80 bg-beige aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                    <img
-                      src={org.image.url}
-                      alt={org.name}
-                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                    />
-                  </div>
-                  <div className="mt-2 flex justify-between">
-                    <div>
-                    <a className="extra-small exception font-medium tracking-wide text-accent uppercase">{org.category}</a>
-                      <h3 className="text-base font-medium hover:underline text-semiblack">
-                        <a href={'/profile/' + org.id}>
-                          <span aria-hidden="true" className="absolute inset-0" />
-                          {org.name}
-                        </a>
-                      </h3>
-                      <p className="mt-1 text-sm text-darkgray pb-1">{org.url}</p>
-                      <p className="text-sm text-gray-700">{org.description.slice(0, 100) + '…'}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 md:mb-8">Profiles</h2>
+            <Search />
           </div>
         </div>
       </section>
