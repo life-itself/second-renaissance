9dd6cd7193a858072c8ebfffb66db56f499b7615
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-17 16:43:49 +0200

    [search,#15][m]: facets are code driven and include counts (but don't yet filter).
    Using itemsjs.

--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Fuse from 'fuse.js'
+import ItemsJS from 'itemsjs'
 
 import orgs from '../data/organization.json'
 import topics from '../data/topic.json'
@@ -9,17 +10,22 @@ import OrgList from '../components/OrgList.js'
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState('')
-  const [searchResults, setSearchResults] = useState([])
-  const sortedSearchResults = searchResults.sort((resultA, resultB) => {
-    return resultA.score - resultB.score;
-  });
-  // extra org from search result
-  // which looks like { item: ..., refIndex: ..., score: ... }
-  // also return all orgs if empty search query
-  var sortedOrgs = orgs
-  if (searchQuery) {
-    sortedOrgs = sortedSearchResults.map((result) => result.item)
+
+  const handleSearch = (searchQuery) => {
+    setSearchQuery(searchQuery)
   }
+
+
+  const facets = [
+    {
+      id: 'topic',
+      name: 'Topic'
+    },
+    {
+      id: 'activity',
+      name: 'Activity'
+    }
+  ]
 
   const searchIndex = new Fuse(orgs, {
     includeScore: true,
@@ -27,15 +33,41 @@ const Search = () => {
     keys: ['name'],
   })
 
-  const handleSearch = (searchQuery) => {
-    setSearchQuery(searchQuery)
-    const results = searchIndex.search(searchQuery)
-    setSearchResults(results)
+  const searchResults = searchIndex.search(searchQuery)
+
+  // configuration for itemsjs faceted search
+  const aggregations = {}
+  for(const f of facets) {
+    aggregations[f.id] = {
+      title: f.name,
+      size: 20, // set to 20 to be bigger than max size of a facet atm
+      conjunction: true  // not sure why
+    }
+  }
+  const itemsjs = ItemsJS(orgs, {
+    native_search_enabled: false,
+    aggregations: aggregations,
+  })
+
+  const sortedSearchResults = searchResults.sort((resultA, resultB) => {
+    return resultA.score - resultB.score;
+  });
+  // orgs from search result
+  // search result is { item: org, refIndex: 1, score: ... }
+  // return all orgs if empty search query
+  var sortedOrgs = orgs
+  if (searchQuery) {
+    sortedOrgs = sortedSearchResults.map((result) => result.item)
   }
 
-  // TODO: compute facet counts for topics and activitys and show them
-  const facets = {
-  }
+  const searchResults2 = itemsjs.search({
+    per_page: 100,
+    ids: sortedOrgs.map(v => v.id)
+  })
+
+  const facetResults = Object.entries(searchResults2.data.aggregations).map(item => {
+    return item[1]
+  })
 
   return (
     <>
@@ -43,57 +75,40 @@ const Search = () => {
         <input
           type="search"
           name="search"
-          className=""
+          placeholder="Search ..."
+          className="inline"
           value={searchQuery}
           onChange={(event) => handleSearch(event.target.value)}
         />
 
-        <p>
+        <p className="inline ml-6">
           Profiles found: {sortedOrgs.length}
         </p>
 
-        <div className="max-w-7xl mx-auto grid grid-cols-2 text-sm">
+        <div className="max-w-7xl mx-auto grid grid-cols-2 text-sm mt-4">
           <div className="grid grid-cols-1 gap-y-10 auto-rows-min md:grid-cols-2 md:gap-x-6">
+            {facetResults.map(facet => (
             <fieldset>
-              <legend className="block font-medium">Topic</legend>
-              <div className="pt-6 space-y-6 sm:pt-4 sm:space-y-4">
-                {topics.map((option, optionIdx) => (
-                  <div key={option.slug} className="flex items-center text-base sm:text-sm">
+              <legend className="block font-medium">{facet.title}</legend>
+              <div className="pt-2 space-y-1">
+                {facet.buckets.map((option, optionIdx) => (
+                  <div key={option.id} className="flex items-center text-base sm:text-sm">
                     <input
-                      id={`price-${optionIdx}`}
-                      name="price[]"
-                      defaultValue={option.slug}
+                      id={`${facet.name}-${optionIdx}`}
+                      name="${facet.name}[]"
+                      defaultValue={option.key}
                       type="checkbox"
                       className="flex-shrink-0 h-4 w-4 border-gray-300 rounded text-yellow-600 focus:ring-yellow-500"
                       defaultChecked={option.checked}
                     />
-                    <label htmlFor={`price-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-600">
-                      {option.name}
+                    <label htmlFor={`${facet.name}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-600">
+                      {option.key} ({option.doc_count})
                     </label>
                   </div>
                 ))}
               </div>
             </fieldset>
-            <fieldset>
-              <legend className="block font-medium">Activity</legend>
-              <div className="pt-6 space-y-6 sm:pt-4 sm:space-y-4">
-                {activitys.map((option, optionIdx) => (
-                  <div key={option.slug} className="flex items-center text-base sm:text-sm">
-                    <input
-                      id={`color-${optionIdx}`}
-                      name="color[]"
-                      defaultValue={option.slug}
-                      type="checkbox"
-                      className="flex-shrink-0 h-4 w-4 border-gray-300 rounded text-yellow-600 focus:ring-yellow-500"
-                      defaultChecked={option.checked}
-                    />
-                    <label htmlFor={`color-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-600">
-                      {option.name}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </fieldset>
+            ))}
           </div>
         </div>
 
