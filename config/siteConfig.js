dd9b788664d8c1f61c5ba4853e28073d2faf0e75
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-31 12:04:44 +0100

    [theme][s]: fix to analytics setup for page route changes.

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -3,7 +3,8 @@ const siteConfig = {
   tagline: 'An ecosystem mapping by Life Itself and collaborators',
   description: 'This site is a tool for exploring an emerging ecosystem. We hope that charting it helps it becomes (more) visible, self-aware, interconnected, resourced and effective. We emphasize this is a work in progress, limited by both our resources and knowledge. We welcome help to improve it.',
   author: 'Life Itself',
-  ownerLogo: '',
+  authorLogo: '',
+  authorUrl: '',
   analytics: 'G-K5529JHP9G',
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,35 @@
+import { useEffect } from 'react'
 import Script from 'next/script'
-import 'tailwindcss/tailwind.css';
+import { useRouter } from 'next/router'
+import { DefaultSeo } from 'next-seo'
+import 'tailwindcss/tailwind.css'
 
 import '../styles/global.css';
 import siteConfig from '../config/siteConfig.js'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
+  // Google Analytics
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  // end Google Analytics
+ 
   return (
     <>
+      {/* TODO: this is not working and can't figure out why 2021-10-23 */}
+      // <DefaultSeo
+      //   titleTemplate={'%s | ' + siteConfig.title}
+      //   defaultTitle={siteConfig.title}
+      //   description={siteConfig.description}
+      //   />
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="afterInteractive"
