life-itself/second-renaissance

commit 2288637729cfd5f1a40917ebbb0dcf91df0c9e80

[#35,db][s]: don't display orgs not selected for round 1 (N or NN in that column).

pages/search.js
@@ -1,19 +1,21 @@
 import Search from "../components/Search.js"
 
-export default function Page() {
+export default function Page({ orgs }) {
   return (
     <>
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <h1 className="mt-16 mb-8 text-4xl">Search</h1>
-        <Search />
+        <Search orgs={orgs} />
       </div>
     </>
   )
 }
 
 export async function getStaticProps(context) {
+  const orgs = await getOrganizations();
   return {
     props: {
+      orgs,
     }
-  };
+  }
 }

components/Search.js
@@ -2,13 +2,12 @@ import React, { useState } from 'react'
 import Fuse from 'fuse.js'
 import ItemsJS from 'itemsjs'
 
-import orgs from '../data/organization.json'
 import topics from '../data/topic.json'
 import activitys from '../data/activity.json'
 import OrgList from '../components/OrgList.js'
 
 
-export default function Search() {
+export default function Search({ orgs }) {
   const facets = [
     {
       id: 'topic',

lib/db.js
@@ -4,6 +4,9 @@ import { existsSync } from 'fs'
 
 export async function getOrganizations() {
   const file = readFileSync('data/organization.json', 'utf8')
-  const result = JSON.parse(file)
+  const all = JSON.parse(file)
+  // filter the ones we are not showing atm
+  console.log(all[0].rd_1_focus.includes('Y'))
+  const result = all.filter(item => !(item.rd_1_focus.includes('N')))
   return result
 }

pages/index.js
@@ -114,7 +114,7 @@ export default function Home({ orgs }) {
         <div className="bg-white">
           <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 md:mb-8">Profiles</h2>
-            <Search />
+            <Search orgs={orgs} />
           </div>
         </div>
       </section>

commit bf276955ea9a8a9499fa5539596ecb0f810ff6a1

[bugfix,pages/search][xs]: fix build broken in 2288637729cfd5f1a40917ebbb0dcf91df0c9e80.

pages/search.js
@@ -1,4 +1,5 @@
 import Search from "../components/Search.js"
+import { getOrganizations } from "../lib/db.js"
 
 export default function Page({ orgs }) {
   return (