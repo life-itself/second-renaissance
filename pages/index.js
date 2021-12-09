d95e6355ed21c7b3207665e96c9c0119250de755
Rufus Pollock <rufus.pollock@datopian.com>
2021-12-09 11:17:03 +0100

    [#31,#44,home,content/outline][l]: update front page with lengthy outline of ecosystem based on latest work.
    * Includes (S)PIE definition of ecosystem
    * Includes core characteristics (revised) from 2020 paper via emerge piece
    * Updates call to action at top in line with new priorities (discover, explore, about)

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { NextSeo } from 'next-seo'
+import fs from 'fs'
+import path from 'path'
+import { MDXRemote } from 'next-mdx-remote'
 
 import { getOrganizations } from "../lib/db.js"
 import Search from "../components/Search.js"
 import config from '../config/siteConfig'
+import parse from '../lib/mdx.js'
 
 
-export default function Home({ orgs }) {
+export default function Home({ orgs, outline }) {
   return (
     <>
       <NextSeo
@@ -82,30 +86,38 @@ export default function Home({ orgs }) {
           <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
             <div className="text-center">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">State of Sensemaking</span>{' '}
-                <span className="block text-yellow-300 xl:inline">Directory</span>
+                <span className="block xl:inline">{config.title}</span>
               </h1>
               <h2 className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                 {config.tagline}
               </h2>
               <p className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 med:text-xl md:max-w-3xl">
-                {config.description} <Link href="/about/"><a className="font-bold underline">Learn more &raquo;</a></Link>
+                {config.description}
               </p>
               <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                 <div className="rounded-md shadow">
                   <a
-                    href="#profiles"
+                    href="#outline"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md bg-yellow-300 hover:bg-yellow-400 md:py-4 md:text-lg md:px-10"
                   >
-                    Browse Profiles
+                    Discover
                   </a>
                 </div>
                 <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                  <Link href="/contribute/">
+                  <Link href="#profiles">
                     <a
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-gray-100 hover:bg-gray-200 md:py-4 md:text-lg md:px-10"
                       >
-                    Contribute
+                      Explore
+                    </a>
+                  </Link>
+                </div>
+                <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                  <Link href="/about/">
+                    <a
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-gray-50 hover:bg-gray-100 md:py-4 md:text-lg md:px-10"
+                      >
+                      About
                     </a>
                   </Link>
                 </div>
@@ -115,10 +127,16 @@ export default function Home({ orgs }) {
         </div>
       </div>
 
+      <section id="outline" className="max-w-md md:max-w-3xl mx-auto px-2 sm:px-6 lg:px-8 mt-24 prose">
+        <MDXRemote {...outline} />
+      </section>
+
       <section id="profiles" className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="bg-white">
           <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 md:mb-8">Profiles</h2>
+            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 md:mb-8 text-center">
+              Profiles
+            </h1>
             <Search orgs={orgs} />
           </div>
         </div>
@@ -129,9 +147,16 @@ export default function Home({ orgs }) {
 
 export async function getStaticProps(context) {
   const orgs = await getOrganizations();
+
+  const CONTENT_PATH = path.join(process.cwd(), 'content/')
+  const postFilePath = path.join(CONTENT_PATH, 'outline.md')
+  const raw = fs.readFileSync(postFilePath)
+  const {mdxSource, frontMatter} =  await parse(raw)
+
   return {
     props: {
       orgs,
+      outline: mdxSource,
     }
   };
 }
