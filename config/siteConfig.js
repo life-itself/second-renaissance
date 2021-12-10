e0f189fe78e62f1934c2f763a584c3d8d78e3fb2
Rufus Pollock <rufus.pollock@datopian.com>
2021-12-10 22:30:58 +0100

    [config][xs]: change Mapping Metamoderism to Mapping Metamodern.

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,7 +1,7 @@
 const siteConfig = {
-  title: 'Mapping Metamodernism',
+  title: 'Mapping Metamodern',
   tagline: 'Charting an emerging ecosystem by Life Itself, Emerge and collaborators',
-  description: 'This project maps an emerging “metamodernist” ecosystem centred on a radical, alternative approach to social change – one that is simultaneously paradigmatic, integrated and engaged. Discover key features and ideas of this growing space. Explore associated organizations, individuals and initiatives. See how it relates to other established and emerging movements.',
+  description: 'This project maps an emerging “metamodern” ecosystem centred on a radical, alternative approach to social change – one that is simultaneously paradigmatic, integrated and engaged. Discover key features and ideas of this growing space. Explore associated organizations, individuals and initiatives. See how it relates to other established and emerging movements.',
   author: 'Life Itself',
   authorLogo: '',
   authorUrl: '',
