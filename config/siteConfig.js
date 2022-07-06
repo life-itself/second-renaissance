08dbcc78890174888ad75f248b2db404f143166a
khalilcodes <khalilali.j@gmail.com>
2022-07-06 14:04:31 +0300

    [configs][s]: add nav links and update user configs

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -13,6 +13,11 @@ const defaultConfig = {
   // Theme
   navLinks: [
     { href: '/about', name: 'About' },
+    { href: '/contribute', name: 'Contribute' },
+    { href: '/glossary/', name: 'Glossary' },
+    { href: 'https://lifeitself.us/ecosystem/snapshot-2020/', name: '2020 Report' },
+    { href: 'https://lifeitself.us/contact', name: 'Contact' },
+    { href: 'https://lifeitself.us/ecosystem/', name: 'A Project of Life Itself & Collaborators' },
   ],
   // optional additional nextSeo content set on each page
   // see https://github.com/garmeeh/next-seo
