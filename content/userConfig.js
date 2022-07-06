0b2fc55a0e1eb99737b5f1c8ceb8e741cea4443b
khalilcodes <khalilali.j@gmail.com>
2022-07-07 01:15:55 +0300

    [configs][xs]: add logo

--- content/userConfig.js
+++ content/userConfig.js
@@ -3,7 +3,7 @@ const userConfig = {
   tagline: 'Charting an emerging ecosystem by Life Itself, Emerge and collaborators',
   description: 'This project maps an emerging “metamodern” ecosystem centred on a radical, alternative approach to social change – one that is simultaneously paradigmatic, integrated and engaged. Discover key features and ideas of this growing space. Explore associated organizations, individuals and initiatives. See how it relates to other established and emerging movements.',
   author: 'Life Itself',
-  authorLogo: '',
+  authorLogo: '/life-itself-logo.svg',
   authorUrl: '',
   analytics: '',
 }
