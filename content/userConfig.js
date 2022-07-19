cdede203eb64d535513f13d8ac0d828e5fbd9ce6
Rufus Pollock <rufus.pollock@datopian.com>
2022-07-19 16:13:22 +0200

    [config][s]: set google analytics again (missed out in changeover).

--- content/userConfig.js
+++ content/userConfig.js
@@ -4,8 +4,8 @@ const userConfig = {
   description: 'This project maps an emerging “metamodern” ecosystem centred on a radical, alternative approach to social change – one that is simultaneously paradigmatic, integrated and engaged. Discover key features and ideas of this growing space. Explore associated organizations, individuals and initiatives. See how it relates to other established and emerging movements.',
   author: 'Life Itself',
   authorLogo: '/life-itself-logo.svg',
-  authorUrl: '',
-  analytics: '',
+  authorUrl: 'https://lifeitself.us/',
+  analytics: 'G-K5529JHP9G',
 }
 
 export default userConfig
