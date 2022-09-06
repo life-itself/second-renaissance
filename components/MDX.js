b9e2cf26cb8871bda5a8c28ae997287abd91e0e5
khalilcodes <khalilali.j@gmail.com>
2022-09-06 04:06:13 +0400

    [mdx][s]: add ecosystem components and fix seo imsge url

--- components/MDX.js
+++ components/MDX.js
@@ -1,21 +1,22 @@
-import { Fragment } from 'react'
-import dynamic from 'next/dynamic'
+import { NextSeo } from 'next-seo'
 import Head from 'next/head'
-import Link from 'next/link'
 import Hero from './Hero'
-import Search from './Search'
-
-const TernaryPlot = dynamic(() => import('./TernaryPlot'))
-const CircularVis = dynamic(() => import('./CircularVis'))
+import CustomLink from './Link'
+import { Pre } from './Pre'
+import ProfileSearch from './ProfileSearch'
+import siteConfig from '../config/siteConfig'
+import TernaryPlot from './TernaryPlot'
+import CircularVis from './CircularVis'
 
 const components = {
   Head,
   Hero,
-  Link,
-  Search,
-  svg: props => <Fragment {...props} />,
+  a: CustomLink,
+  pre: Pre,
+  ProfileSearch,
   TernaryPlot,
   CircularVis,
+  GlobalTest: ({children}) => <h1 className="bg-red-300">{children}</h1>,
   wrapper: ({ layout, ...rest }) => {
     const Layout = require(`../layouts/${layout}`).default
     return <Layout {...rest} />
@@ -23,13 +24,37 @@ const components = {
 }
 
 export default function MdxPage({ children, ...rest }) {
-  const { Component, frontMatter } = children;
+  const { Component, frontMatter } = children
+  const websiteUrl = siteConfig.authorUrl.replace(/\/+$/, '')
+  const seoImageUrl = frontMatter?.image?.url?.startsWith("http") 
+    ? frontMatter.image.url
+    : websiteUrl + frontMatter.image
+  
   return (
-    <Component
-      layout={frontMatter.layout}
-      components={components}
-      frontMatter={frontMatter}
-      {...rest}
-    />
+    <>
+      <NextSeo 
+        title={frontMatter.title}
+        openGraph={{
+          title: frontMatter.title,
+          description: frontMatter.description,
+          images: frontMatter.image
+            ? [
+                {
+                  url: seoImageUrl,
+                  width: 1200,
+                  height: 627,
+                  alt: frontMatter.title
+                }
+              ] 
+            : siteConfig?.nextSeo?.openGraph?.images || [],
+        }}
+      />
+      <Component
+        layout={frontMatter.layout}
+        components={components}
+        frontMatter={frontMatter}
+        {...rest}
+      />
+    </>
   )
 }
