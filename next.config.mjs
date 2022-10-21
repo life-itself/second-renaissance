40b2a2a3808e1bf407331ae83f55fa57117d5f65
khalilcodes <khalilali.j@gmail.com>
2022-10-21 17:22:54 +0400

    disable eslint on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 import { withContentlayer } from "next-contentlayer";
 
 export default withContentlayer({
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.infrastructureLogging = {
       level: "error",
