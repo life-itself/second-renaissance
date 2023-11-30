3dde68ce7d278c528e0dfbbb16fcebe64180235e
Rufus Pollock <rufus.pollock@datopian.com>
2023-11-30 21:11:47 +0100

    [config][xs]: tidy up navbar removing a currently dead link and renaming contribute.

--- content/config.mjs
+++ content/config.mjs
@@ -7,11 +7,10 @@ const userConfig = {
   analytics: 'G-K5529JHP9G',
   navLinks: [
     { href: '/about', name: 'About' },
-    { href: '/contribute', name: 'Contribute' },
+    { href: '/contribute', name: 'Participate' },
     { href: '/glossary/', name: 'Glossary' },
-    { href: 'https://lifeitself.us/ecosystem/snapshot-2020/', name: '2020 Report' },
-    { href: 'https://lifeitself.us/contact', name: 'Contact' },
-    { href: 'https://lifeitself.us/ecosystem/', name: 'A Project of Life Itself & Collaborators' },
+    { href: 'https://lifeitself.org/contact', name: 'Contact' },
+    { href: 'https://lifeitself.org/', name: 'A Project of Life Itself & Collaborators' },
   ],
   theme: {
     default: ''
