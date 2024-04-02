life-itself/second-renaissance

commit c8a5114f1d8db3ac1296e25e2f16e448a93bdbb2

[#249,ecosystem,refactor][s]: move 2021 ecosystem work from /ecosystem to /ecosystem/pip-2021.
- Stub a new base ecosystem page in markdown.
- Small tweaks to ecosystem.tsx page for getting profiles and lib/computeFields.ts to correct for new url location of profiles etc

pages/ecosystem/pip-2021.tsx
@@ -132,8 +132,10 @@ export const getStaticProps: GetStaticProps = async (): Promise<
 
     const mddb = await clientPromise;
 
-    const profileFiles = await mddb.getFiles({ folder: "profiles" });
+    // TODO: this should really be a library function ...
+    const profileFiles = await mddb.getFiles({ folder: "ecosystem/pip-2021/profiles" });
     const profiles = profileFiles.reduce((acc, file) => {
+        // TODO: 2024-04-02 can probably remove now ...
         // TODO temporary hack - some of the profiles (created by Matthew) have incorrect metadata
         if (!hasRequiredProfileFields(file.metadata)) {
             return acc;
@@ -144,7 +146,7 @@ export const getStaticProps: GetStaticProps = async (): Promise<
         acc.push({
             ...file.metadata,
             image: file.metadata.image?.url ?? file.metadata.image,
-            urlPath: file.url_path,
+            urlPath: '/' + file.url_path,
         });
         return acc;
     }, []);

commit c4dae7a4a20ec860a6675ecb5f6aa04daa3a9716

[bugfix,addendum][xs]: missed breakage in c8a5114f1d8db3ac1296e25e2f16e448a93bdbb2.
Only showed up in `build` but not `dev`.

pages/ecosystem/pip-2021.tsx
@@ -9,7 +9,7 @@ import CircularVis from "@/components/custom/CircularVis"
 import ProfileSearch from "@/components/custom/ProfileSearch"
 import MdxComponent from "@/components/MdxComponent";
 
-import type { CustomAppProps } from "./_app";
+import type { CustomAppProps } from "../_app";
 import { hasRequiredProfileFields } from "@/lib/temp/hasRequiredProfileFields";
 
 