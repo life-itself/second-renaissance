70b8095d2434cc99993471cf9704a63b0873e60c
Rufus Pollock <rufus@lifeitself.us>
2024-04-08 10:00:10 +0000

    [home][s]: update hero with new text and new image.

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,9 @@ const Hero = () => (
   <>
   <div className="relative isolate overflow-hidden pt-14">
     <img
-      src="/img/homepage/hero-bg.jpg"
+      src="/img/homepage/sun-spoor-1874.webp"
       alt=""
-      className="absolute inset-0 -z-10 h-full w-full object-cover"
+      className="absolute inset-0 -z-10 h-full w-full object-cover bg-slate-700 opacity-40"
     />
     <div className="mx-auto max-w-4xl py-12 sm:py-16 lg:py-20 text-center">
       <h1 className="text-2xl tracking-tight sm:text-5xl font-serif">
@@ -13,16 +13,18 @@ const Hero = () => (
       <h1 className="text-4xl font-bold tracking-tight sm:text-7xl mt-8">
         The Second Renaissance
       </h1>
-      <p className="mt-4 text-3xl font-serif">
-        It is a time between worlds, as we witness the breakdown<br />of the old paradigm and the birth of the new<br />
-        – moving from modernity into what comes next. 
-      </p>
-      <p className="mt-4 text-3xl font-serif">
-        Learn more and the practical ways to contribute through articles, courses and community.
-      </p>
+      <div className="mt-4 text-3xl font-serif">
+        <p>
+          The old world is dying and a new world is yet to be born. The civilization crisis we are living is also precursor of an awakening.
+          <br /><br />
+          From the old paradigm of modernity to the birth of a new dawn for humanity.
+          <br /><br />
+          Come discover and dream with others.
+        </p>
+      </div>
       <div className="mt-10 flex items-center justify-center gap-x-6">
         <a href="#next" className="text-sm font-semibold leading-6">
-          Read more
+          Continue
           <br />
           <span aria-hidden="true">↓</span>
         </a>
