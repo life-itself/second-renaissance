7a6c2cf01e6c0a9d433695e573db5eb77778f4ac
Rufus Pollock <rufus@lifeitself.us>
2024-06-05 14:47:45 +0000

    [ecosystem][s]: add caption to map and link to new post about it.

--- pages/ecosystem.jsx
+++ pages/ecosystem.jsx
@@ -77,6 +77,7 @@ const Graphic = () => (
       src="/assets/img/ecosystem/second-renaissance-ecosystem-map.webp"
       alt="Map of the Second Renaissance Changemaking Ecosystem as created by Life Itself and friends May 2024"
     />
+    <caption className="text-sm text-center mx-auto max-w-3xl block mt-5">An impressionistic map of the second renaissance "paradigmatic changemakers" ecosystem. It shows some of the different communities, movements and spaces (primarily in the Global North) that aspire to and/or act to foster paradigmatic change with a second renaissance perspective <a className="underline" href="https://secondrenaissance.substack.com/p/map-of-second-renaissance-changemaking">Read more &raquo;</a></caption>
   </figure>
 );
 
