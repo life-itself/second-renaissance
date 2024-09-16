cde127245a78564293e2e5eea5ba09677159d477
Shahzaib Khan <shahzaib198@gmail.com>
2024-09-16 15:23:01 +0500

    [ADD] Pages update

--- pages/art.jsx
+++ pages/art.jsx
@@ -3,26 +3,30 @@ const Page = () => {
     <>
       <Hero></Hero>
       <Catalyst></Catalyst>
+      <SecondRenaissance />
       <Manifesto></Manifesto>
     </>
   );
 };
 
 const Hero = () => (
-  <div className="relative isolate overflow-hidden pt-24 sm:pt-32 min-h-screen bg-black">
+  <div
+    className="relative isolate overflow-hidden pt-24 sm:pt-32 bg-center bg-cover min-h-screen bg-black"
+    style={{
+      backgroundImage: "url('/assets/img/art/trees.webp')",
+      backgroundPosition: "center",
+      backgroundSize: "cover"
+    }}>
     <div className="mx-auto max-w-7xl md:px-6 lg:px-8 text-white">
-      <img
-        src="/assets/img/art/trees.webp"
-        alt=""
-        className="absolute inset-0 -z-10 object-cover opacity-90"
-        />
+      
       <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
         <div className="px-6 lg:px-0 lg:pr-8 lg:pt-4">
           <h1 className="mt-10 text-4xl font-bold tracking-tight sm:text-6xl">
             Art of the second renaissance
           </h1>
           <h3 className="mt-6 text-2xl leading-8 sm:text-4xl">
-            We are an artists collective making art to bring forth a second renaissance.
+            We are an artists collective making art to bring forth a second
+            renaissance.
           </h3>
         </div>
         <div className="sm:px-6 lg:px-0">
@@ -33,8 +37,7 @@ const Hero = () => (
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen>
-            </iframe>
+            allowfullscreen></iframe>
         </div>
       </div>
       <div className="mt-32 flex items-center justify-center gap-x-6 text-center">
@@ -46,103 +49,214 @@ const Hero = () => (
       </div>
     </div>
   </div>
-)
+);
 
 const Catalyst = () => (
   <div id="catalyst" className="overflow-hidden">
-    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-10 mb-10">
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div className="lg:pr-8 lg:pt-8">
           <div className="lg:max-w-lg">
-            <h1 className="mt-10 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl font-serif">
-             Art as a catalyst for transformation
+            <h1 className="mt-10 text-3xl font-inter font-bold  text-gray-900 ">
+              Art as a catalyst for transformation
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Art is an expression of our consciousness and humanity, from the prehistoric cave paintings to the graffiti in the streets of our towns, art has always been a gateway to transformation.
+            <p className="mt-6 text-xl leading-8">
+              Art is an expression of our consciousness and humanity, from the
+              prehistoric cave paintings to the graffiti in the streets of our
+              towns, art has always been a gateway to transformation.
             </p>
-            <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-3xl font-serif">
+            <h3 className="mt-10 text-xl font-bold  text-gray-900 sm:text-3xl ">
               Impermanence: our first theme
             </h3>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Each year we curate an exhibition and a magazine to share with the world the artistic embodiment of transformation for a Second Renaissance. Our latest exhibition is on the theme of Impermanence and  took place in Thénac France in April 2024.
+            <p className="mt-6 text-xl leading-8">
+              Each year we curate an exhibition and a magazine to share with the
+              world the artistic embodiment of transformation for a Second
+              Renaissance. Our latest exhibition is on the theme of Impermanence
+              and took place in Thénac France in April 2024.
             </p>
             <p className="mt-8">
-              <a
-                href="/art/impermanence"
-                className="btn btn-blue"
-                >
-                See the magazine and exhibition &raquo;
+              <a href="/assets/" className="btnsq btn-black text-3xl">
+                See the exhibition and magazine
               </a>
             </p>
           </div>
         </div>
         <div className="pt-24">
-          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/BCNPMCZ_leI?si=q8Al5Oz4RCks6Pb6&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-          </iframe>
+          <iframe
+            className="aspect-video w-full"
+            src="https://www.youtube.com/embed/BCNPMCZ_leI?si=q8Al5Oz4RCks6Pb6&rel=0&modestbranding=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen></iframe>
         </div>
       </div>
     </div>
   </div>
-)
+);
+
+const SecondRenaissance = () => (
+  <div id="SecondRenaissance" className="overflow-hidden">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-20 mb-10">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="lg:pr-8 lg:pt-8">
+          <div className="lg:max-w-lg">
+            <h2 className="text-5xl mb-6">
+              Art of the
+              <br />
+              Second
+              <br />
+              Renaissance
+            </h2>
+            <h2 className="text-xl font-bold text-red-500 mb-4">
+              Manifesto & Essay
+            </h2>
+            <p className="mb-4 text-xl leading-8">
+              Seeking an artistic tribe for an emerging cultural paradigm.
+            </p>
+            <p className="mb-6 text-xl leading-8">
+              Marrying the artistic, political and spiritual world.
+            </p>
+            <p className="mt-8">
+              <a href="/assets/" className="btnsq btn-black text-3xl">
+              Download
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="pt-24">
+          <h2 className="text-3xl font-bold mb-4">The philosophy behind</h2>
+          <p className="mt-6 text-xl leading-8">
+            We have written a manifesto and essay to explain in more detail the
+            philosophy behind the Art of the Second Renaissance. This is in the
+            context of forming an art collective for the emerging cultural
+            paradigm, which marries the artistic, political and spiritual world.
+          </p>
+          <p className="mt-6 text-xl leading-8">
+            Subscribe for updates about art of the second renaissance
+            (quarterly).
+          </p>
+          <p className="mt-8">
+            <a href="/assets/" className="btnsq btn-black text-3xl">
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 const Manifesto = () => (
   <div className="bg-white px-6 py-32 lg:px-8">
     <div className="mx-auto max-w-3xl text-base leading-7 text-gray-600 text-center prose text-lg leading-8">
-      <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center font-serif mb-8">Manifesto</h1>
+      <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center font-serif mb-8">
+        Manifesto
+      </h1>
 
-      <p>We are a collective of artists making art to bring forth the second renaissance.</p>
       <p>
-        We are alchemists<br />
-        We create a new world through our work.<br />
+        We are a collective of artists making art to bring forth the second
+        renaissance.
+      </p>
+      <p>
+        We are alchemists
+        <br />
+        We create a new world through our work.
+        <br />
         We are the bridge between the dying past and the future unborn.
       </p>
-      <p>Art is an expression of our consciousness and humanity, this is our path of transformation. Life as an art.</p>
-      <p>So what will be an Art for the Second Renaissance? It is the dawn, calling us to value interbeingness, awakening, wisdom and inner-evolution. Because we believe and witness that there is an innate calling in life to be deeply and truly free. A freedom which includes and transcends the freedom to have, to feel and to think. A freedom to be and become. It is a Liberation.</p>
-      <p>That this life movement can not be repressed by fear, war, terror, death, dictatorship. That our ability to understand, love, care and contribute is an expression of our intuitive knowledge of the truth of our interbeingness. By doing so we become the author of tomorrow's future and the priests to the dying past.</p>
+      <p>
+        Art is an expression of our consciousness and humanity, this is our path
+        of transformation. Life as an art.
+      </p>
+      <p>
+        So what will be an Art for the Second Renaissance? It is the dawn,
+        calling us to value interbeingness, awakening, wisdom and
+        inner-evolution. Because we believe and witness that there is an innate
+        calling in life to be deeply and truly free. A freedom which includes
+        and transcends the freedom to have, to feel and to think. A freedom to
+        be and become. It is a Liberation.
+      </p>
+      <p>
+        That this life movement can not be repressed by fear, war, terror,
+        death, dictatorship. That our ability to understand, love, care and
+        contribute is an expression of our intuitive knowledge of the truth of
+        our interbeingness. By doing so we become the author of tomorrow's
+        future and the priests to the dying past.
+      </p>
 
       <ul className="list-none mt-16">
-      <li>Our work is to create a space to be with all of it. To be with the joy, the darkness, the hope, the sadness, the unknown.</li>
-      <li>Our work is to inquire about what would a world that works for everyone look like?</li>
-      <li>What would be asked of us to let go and develop for this to be fulfilled?</li>
-      <li>Our work is a practice to inquire about the nature of reality.</li>
-      <li>Our work is mystical, political, spiritual, religious and artistic.</li>
-      <li>Our work inquires about the good through community.</li>
-      <li>Our work expresses beauty through art.</li>
-      <li>Our work  inquires about the truth through the practice of wisdom.</li>
-      <li>Our work is grounded in practice.</li>
-      <li>Our work stands to liberate ourselves from nihilism and relativism.  </li>
-      <li>Our work stands to go beyond the limitation of the superiority, equality and inferiority complex.  </li>
-      <li>Our work embraces ethics.  </li>
-      <li>Our work looks for the middle way in the Buddhist sense, not as a pursuit of the ‘average’  </li>
-      <li>Our work explores impermanence and eternity.  </li>
-      <li>Our work is a practice of the power authenticity with oneself.   </li>
-      <li>Our work is a demonstration of the power of vulnerability.  </li>
-      <li>Our work is a practice of being with our cognitive dissonance.  </li>
-      <li>Our work is expressed and unapologetic   </li>
-      <li>Our work is understanding and compassionate.  </li>
-      <li>Our work is a practice of non-self and interbeingness.  </li>
-      <li>Our work is contemplative and activist at the same time.  </li>
-      <li>Our work is a practice of liberation.  </li>
-      <li>Our work is life as an art.  </li>
-      <li>Our work is ritualistic.  </li>
-      <li>Our work is the practice of equanimity.  </li>
-      <li>Our work touches the ultimate dimension and links it to the relative world.  </li>
-      <li>Our work are prayers.  </li>
-      <li>Our work is an embrace to the earth and mother nature.</li>
+        <li>
+          Our work is to create a space to be with all of it. To be with the
+          joy, the darkness, the hope, the sadness, the unknown.
+        </li>
+        <li>
+          Our work is to inquire about what would a world that works for
+          everyone look like?
+        </li>
+        <li>
+          What would be asked of us to let go and develop for this to be
+          fulfilled?
+        </li>
+        <li>Our work is a practice to inquire about the nature of reality.</li>
+        <li>
+          Our work is mystical, political, spiritual, religious and artistic.
+        </li>
+        <li>Our work inquires about the good through community.</li>
+        <li>Our work expresses beauty through art.</li>
+        <li>
+          Our work inquires about the truth through the practice of wisdom.
+        </li>
+        <li>Our work is grounded in practice.</li>
+        <li>
+          Our work stands to liberate ourselves from nihilism and relativism.{" "}
+        </li>
+        <li>
+          Our work stands to go beyond the limitation of the superiority,
+          equality and inferiority complex.{" "}
+        </li>
+        <li>Our work embraces ethics. </li>
+        <li>
+          Our work looks for the middle way in the Buddhist sense, not as a
+          pursuit of the ‘average’{" "}
+        </li>
+        <li>Our work explores impermanence and eternity. </li>
+        <li>Our work is a practice of the power authenticity with oneself. </li>
+        <li>Our work is a demonstration of the power of vulnerability. </li>
+        <li>Our work is a practice of being with our cognitive dissonance. </li>
+        <li>Our work is expressed and unapologetic </li>
+        <li>Our work is understanding and compassionate. </li>
+        <li>Our work is a practice of non-self and interbeingness. </li>
+        <li>Our work is contemplative and activist at the same time. </li>
+        <li>Our work is a practice of liberation. </li>
+        <li>Our work is life as an art. </li>
+        <li>Our work is ritualistic. </li>
+        <li>Our work is the practice of equanimity. </li>
+        <li>
+          Our work touches the ultimate dimension and links it to the relative
+          world.{" "}
+        </li>
+        <li>Our work are prayers. </li>
+        <li>Our work is an embrace to the earth and mother nature.</li>
       </ul>
+      <p className="mt-8">
+        <a href="/assets/" className="btnsq btn-black text-3xl">
+          Download
+        </a>
+      </p>
     </div>
   </div>
-)
+);
 
 export function getStaticProps() {
   return {
     props: {
       meta: {
-        layout: 'unstyled'
-      }
-    }
-  }
+        layout: "unstyled",
+      },
+    },
+  };
 }
 
 export default Page;
-
--- pages/art/impermanence.jsx
+++ pages/art/impermanence.jsx
@@ -1,3 +1,5 @@
+import ImpermanenceArtVideo from "../../components/ImpermanenceArtVideo";
+
 const Page = () => {
   return (
     <>
@@ -10,10 +12,10 @@ export function getStaticProps() {
   return {
     props: {
       meta: {
-        layout: 'unstyled'
-      }
-    }
-  }
+        layout: "unstyled",
+      },
+    },
+  };
 }
 
 export default Page;
@@ -24,30 +26,184 @@ const Hero = () => (
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div className="lg:pr-8 lg:pt-56">
           <div className="lg:max-w-lg">
-            <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl hidden">
+            <h1 className="font-normal text-4xl leading-[50px]">
               Magazine and Exhibition
             </h1>
-            <p className="mt-6 text-2xl leading-8">
-              For our first magazine and exhibition we chose the theme of Impermanence. Across religions there is often the practice of sitting with the impermanent nature of life, from the five remembrance to the memento mori, these contemplation allows us to grow our wisdom and insight of inter-connectedness.
+            <p className="mt-6 text-xl leading-8">
+              For our first magazine issue and exhibition we chose the theme of
+              Impermanence. Across religions there is often the practice of
+              sitting with the impermanent nature of life, from the five
+              remembrance to the memento mori, these contemplation allows us to
+              grow our wisdom and insight of inter-connectedness.
             </p>
             <p className="mt-8">
-              <a
-                href="/assets/"
-                className="btnsq btn-black text-3xl"
-                >
-                Download magazine
+              <a href="/assets/" className="btnsq btn-black text-3xl">
+                Download our magazine
               </a>
             </p>
           </div>
         </div>
-        <div className="flex items-start justify-end order-first">
+        <div className="flex items-start justify-center order-first">
           <img
             src="/assets/img/art/impermanence-cover.webp"
             alt=""
-            className="max-w-none mx-auto h-screen"
+            className="mx-auto  max-w-[100%]"
           />
         </div>
       </div>
     </div>
+    <div className="container mx-auto ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+        <div className="">
+          <img
+            src="/assets/img/art/impermanence-1.webp"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/assets/img/art/impermanence-2.webp"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/assets/img/art/impermanence-3.webp"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="/assets/img/art/impermanence-4.webp"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+    <div className="container mx-auto mt-20 mb-20">
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-7/12">
+          <p className="text-xl leading-8">
+            We don’t want to just talk about the 2nd Renaissance, we want to
+            make it happen. We want to make this new culture come to live. What
+            better way to birth a new cultural paradigm than through art?
+            <br />
+            <br />
+            This is the first issue of the Second Renaissance Magazine on the
+            theme of impermanence. It is only possible through the  contribution
+            of the community work and time. We are grateful to all the
+            contributors. The magazine is grounded under the Art of the Second
+            Renaissance manifesto and essay.
+            <br />
+            <br />
+            <em>
+              If you wish to contribute works for our next issue Mythos please
+              write to sylvie@lifeitself.org
+            </em>
+          </p>
+
+          <p className="mt-8">
+            <a href="/assets/" className="btnsq btn-black text-3xl">
+              Art of the Second Renaissance
+            </a>
+          </p>
+        </div>
+        <div className="md:w-4/12 flex flex-col items-center">
+          <img
+            src="/assets/img/art/impermanence-cover.webp"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <p className="mt-[-50px]">
+            <a href="/assets/" className="btnsq btn-black text-3xl">
+              Download
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="container mx-auto mt-20 mb-20">
+      <div className="block">
+        <div className="w-full">
+          <h2 className="text-center text-2xl md:text-7xl h-[84.72px] text-bold font-inter font-normal">
+            View the Impermanence Exhibition
+          </h2>
+        </div>
+      </div>
+    </div>
+    <ImpermanenceArtVideo
+      heading=" The art of impermanence"
+      description={
+        "We are taken through the intention behind the exhibition curated by Life Itself co-founder and artist Sylvie Barbier. She shares the importance of the spiritual practice in this time of uncertainty through the art of sitting with impermanence."
+      }
+      video={"BCNPMCZ_leI"}
+    />
+    <ImpermanenceArtVideo
+      heading="Grim Reaper - with Luc Warring"
+      description={
+        "Luc Waring share his relationship with death from his mindfulness practice through series of paintings of the Grim Reapers."
+      }
+      video={"dzZJ0X0CkrI"}
+    />
+    <ImpermanenceArtVideo
+      heading="I give you my word - with Paz Perlman"
+      description={
+        'Paz Perlman share the story and meaning behind the art work " I give you my word" which was part of the Art of impermanence exhibition.'
+      }
+      video={"ttY24srAYI0"}
+    />
+    <ImpermanenceArtVideo
+      heading="I'm of the nature to die - with Sylvie Barbier"
+      description={
+        "Sylvie Barbier here share her journey of navigating grief and peace with to coexistence of the death of her father and her coming child which inspired the theme of the exhibition on impermanence."
+      }
+      video={"ttY24srAYI0"}
+    />
+    <ImpermanenceArtVideo
+      heading="Metamorphosis - with Paul Tingen"
+      description={
+        "Paul Tingen share the relationship between his music and the buddhist teaching around impermanence."
+      }
+      video={"MP3b-O9f8_k"}
+    />
+    <ImpermanenceArtVideo
+      heading="Nothing will ever stop the music - with Max Pugh"
+      description={
+        'Max Pugh shares about his contemplation of impermanence through his video installation work "Nothing will ever stop the music" made in 2022 after the pandemic.'
+      }
+      video={"m4QQRfKIMjU"}
+    />
+    <ImpermanenceArtVideo
+      heading="Cosmic Birth- by Sylvie Barbier"
+      description={
+        "the performance is the first act of Metamorphose a transformative opera by Sylvie Barbier"
+      }
+      video={"lWoUdIikBZI"}
+    />
+    <ImpermanenceArtVideo
+      heading="Phoenix - by Sylvie Barbier"
+      description={
+        "The artist went to record the damages of a wild fire which took place in France. She contrast the destruction with the innocence of her 2 years old son. The fire is a direct reminder of the impermanent nature of life."
+      }
+      video={"QNIMVEdTEEg"}
+    />
+    <ImpermanenceArtVideo
+      heading="Ashes to ashes- By Sylvie Barbier (short)"
+      description={
+        "The artist performs a ritual in a cave along the Dordogne river (home of many prehistoric cave painting sites) to honour the life and death of her father."
+      }
+      video={"Llon-fUFNtU"}
+    />
+    <ImpermanenceArtVideo
+      heading="Ashes to ashes- By Sylvie Barbier (long)"
+      description={
+        "The artist performs a ritual in a cave along the Dordogne river (home of many prehistoric cave painting sites) to honour the life and death of her father."
+      }
+      video={"obD1Jj80ajw"}
+    />
   </div>
 );
