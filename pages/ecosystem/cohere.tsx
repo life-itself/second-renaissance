f15394b92514fec7b32a80769ab5079fafbf5b87
catherinet1 <97066487+catherinet1@users.noreply.github.com>
2024-10-07 12:00:40 +0200

    Update cohere.tsx
    Add EU disclaimer

--- pages/ecosystem/cohere.tsx
+++ pages/ecosystem/cohere.tsx
@@ -74,7 +74,8 @@ const Image = () => (
 const Cohere = () => (
     <div id="cohere" className="mt-12 sm:mt-24 prose max-w-5xl mx-auto">
             <p>This mapping was co-funded by the European Union as part of the Cohere+ project, in collaboration with: The Hague Center, Institute for Integral Studies, Emerge, and Ekskaret.</p>
-            <img
+            <p>Views and opinions expressed are however those of the author(s) only and do not necessarily reflect those of the European Union or the European Education and Culture Executive Agency (EACEA). Neither the European Union nor EACEA can be held responsible for them.</p>
+        <img
         className="bg-gray-50 mx-auto"
         src="/assets/cohere and co-funded EU logo xs.jpg"
         alt=""
